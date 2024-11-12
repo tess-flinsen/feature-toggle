23fed64eef7c7daf175183d7393940db3f94513e
Anastasiia Munteanu <munteanu.anastasiia@gmail.com>
2024-11-13 00:14:23 +0100

    Initial commit

---
+++ src/test/java/com/example/featuretoggle/FeaturetoggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.featuretoggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturetoggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
