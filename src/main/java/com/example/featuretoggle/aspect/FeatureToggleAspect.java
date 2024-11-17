403983b1c672d88bb8698cbcdd433da3b4098813
Anastasiia Munteanu <munteanu.anastasiia@gmail.com>
2024-11-17 23:23:19 +0100

    fix: Add only Before annotation in Aspect class

--- src/main/java/com/example/featuretoggle/aspect/FeatureToggleAspect.java
+++ src/main/java/com/example/featuretoggle/aspect/FeatureToggleAspect.java
@@ -1,8 +1,6 @@
 package com.example.featuretoggle.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,7 +8,7 @@ import org.springframework.stereotype.Component;
 import com.example.featuretoggle.FeatureToggles;
 import com.example.featuretoggle.annotation.FeatureToggle;
 import com.example.featuretoggle.exception.FeatureToggleNotEnabledException;
-import com.example.featuretoggle.service.impl.FeatureToggleServiceImpl;
+import com.example.featuretoggle.service.FeatureToggleService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,28 +18,14 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 public class FeatureToggleAspect {
-  private final FeatureToggleServiceImpl featureToggleService;
-
-    @Around(value = "@annotation(featureToggle)")
-    public Object checkFeatureToggleAnnotation(ProceedingJoinPoint joinPoint, FeatureToggle featureToggle) throws Throwable {
-        return checkToggle(joinPoint, featureToggle);
-    }
-
-    private Object checkToggle(ProceedingJoinPoint joinPoint, FeatureToggle featureToggle) throws Throwable {
-        FeatureToggles toggle = featureToggle.value();
-
-        if (featureToggleService.check(toggle.getFeatureName())) {
-            return joinPoint.proceed();
-        }
-        log.warn("Feature toggle {} is not enabled!", toggle.getFeatureName());
-        throw new FeatureToggleNotEnabledException(toggle.getFeatureName());
-    }
-
+    private final FeatureToggleService featureToggleService;
     @Before("@annotation(featureToggle)")
     public void checkFeatureToggle(JoinPoint joinPoint, FeatureToggle featureToggle) {
         FeatureToggles toggle = featureToggle.value();
+
         if (!featureToggleService.check(toggle.getFeatureName())) {
-        throw new FeatureToggleNotEnabledException(toggle.getFeatureName());
+            log.warn("Feature toggle {} is not enabled!", toggle.getFeatureName());
+            throw new FeatureToggleNotEnabledException(toggle.getFeatureName());
         }
     }
 }
